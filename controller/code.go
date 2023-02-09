32600e9350fc3ed7fd98048ffec3e8a7acbe224a
KiceAmber <kiceamber@gmail.com>
2023-02-09 20:08:21 +0800

    完善响应码Code结构

--- controller/code.go
+++ controller/code.go
@@ -4,12 +4,16 @@ type RespCode int32
 
 const (
 	CodeSuccess RespCode = 1000 + iota
+	CodeInvalidParams
+	CodeUserExist
 )
 
 var CodeMapMsg = map[RespCode]string{
-	CodeSuccess: "成功",
+	CodeSuccess:       "成功",
+	CodeInvalidParams: "参数不足或错误",
+	CodeUserExist:     "用户存在",
 }
 
-func (code RespCode) GetMapping() string {
+func (code RespCode) getMapping() string {
 	return CodeMapMsg[code]
 }
