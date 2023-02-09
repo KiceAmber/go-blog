b400e4539a4c234b23355a1db614eacdbb9c7aef
KiceAmber <kiceamber@gmail.com>
2023-02-09 16:27:05 +0800

    完善响应结构体

--- controller/response.go
+++ controller/response.go
@@ -6,13 +6,13 @@ import (
 )
 
 type RespData struct {
-	Code    RespCode
-	Message string
-	Data    interface{}
+	Code    RespCode    `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 func RespError(c *gin.Context, code RespCode, data interface{}) {
-	c.JSON(http.StatusOK, RespData{
+	c.JSON(http.StatusOK, &RespData{
 		Code:    code,
 		Message: code.getMapping(),
 		Data:    data,
@@ -20,7 +20,7 @@ func RespError(c *gin.Context, code RespCode, data interface{}) {
 }
 
 func RespSuccess(c *gin.Context, data interface{}) {
-	c.JSON(http.StatusOK, RespData{
+	c.JSON(http.StatusOK, &RespData{
 		Code:    CodeSuccess,
 		Message: CodeSuccess.getMapping(),
 		Data:    data,
