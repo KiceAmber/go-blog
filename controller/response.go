3e518d2f5b4214c63a2d5c14afa9b8c88d27f7db
KiceAmber <kiceamber@gmail.com>
2023-02-09 16:22:38 +0800

    完善响应结构体

--- controller/response.go
+++ controller/response.go
@@ -1,1 +1,28 @@
 package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type RespData struct {
+	Code    RespCode
+	Message string
+	Data    interface{}
+}
+
+func RespError(c *gin.Context, code RespCode, data interface{}) {
+	c.JSON(http.StatusOK, RespData{
+		Code:    code,
+		Message: code.getMapping(),
+		Data:    data,
+	})
+}
+
+func RespSuccess(c *gin.Context, data interface{}) {
+	c.JSON(http.StatusOK, RespData{
+		Code:    CodeSuccess,
+		Message: CodeSuccess.getMapping(),
+		Data:    data,
+	})
+}
