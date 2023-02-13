e7615d63af02cfaf5f0f8ef1b624568c0f039dbd
KiceAmber <kiceamber@gmail.com>
2023-02-13 11:16:25 +0800

    新增文章表

--- repository/create_table.sql
+++ repository/create_table.sql
@@ -2,6 +2,9 @@ CREATE DATABASE `go_blog`;
 
 USE `go_blog`;
 
+-- ---------------
+--   用户信息表
+-- ---------------
 CREATE TABLE `user` (
         `id` BIGINT(10) AUTO_INCREMENT NOT NULL PRIMARY KEY,
         `user_id` BIGINT(20) NOT NULL,
@@ -13,4 +16,20 @@ CREATE TABLE `user` (
         `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         UNIQUE INDEX `idx_user_id` (`user_id`),
         UNIQUE INDEX `idx_username` (`username`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 collate=utf8mb4_general_ci;
+
+
+-- ---------------
+--   文章表
+-- ---------------
+CREATE TABLE `article` (
+        `id` BIGINT(10) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        `article_id` BIGINT(20) NOT NULL,
+        `title` VARCHAR(64) NOT NULL,
+        `content` LONGTEXT NOT NULL,
+        `cover_image` VARCHAR(64) NOT NULL,
+        `author_id` BIGINT(20) NOT NULL,
+        `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+        `update_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        UNIQUE INDEX `idx_title` (`title`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8mb4 collate=utf8mb4_general_ci;
